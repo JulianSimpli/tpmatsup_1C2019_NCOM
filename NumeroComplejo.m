2de058960eb975eff344863df01a4772ddceabe8
mnigliazzo <49684313+mnigliazzo@users.noreply.github.com>
2019-04-20 18:06:28 -0300

    Add files via upload

--- NumeroComplejo.m
+++ NumeroComplejo.m
@@ -6,7 +6,7 @@ classdef NumeroComplejo<handle
        FormBinomica_real % parte real de la forma Binómica
        FormBinomica_img % parte imaginaria de la forma Binómica
        FormPolar_ro   % equivale al módulo del número complejo de la forma Binómica
-       FomrPolar_fi   % equivale al ángulo que forma el vector con el semieje positivo
+       FormPolar_fi   % equivale al ángulo que forma el vector con el semieje positivo
        FormBinomica_string  % forma Binómica completa incluye la i
        FormPolar_string  %forma polar completa ejemplo [5;0.7854] ángulo en radianes
        FormParOrdenado_string    % forma par ordenado, ejemplo (FormBinomica_real;FormBinomica_img)   
@@ -20,7 +20,7 @@ classdef NumeroComplejo<handle
        function tipo=dimeTipoComplejo(thisnumeroComplejo, val)
            tipo=0;  %no es complejo
             %validar que val es complejo
-            if(~isempty(regexp(val,'^\((+|-)?\d+(\.\d+)?;(+|-)?\d+(\.\d+)?\)$','match')))
+            if(~isempty(regexp(val,'^\((+|-)?\d+(\.\d+)?,(+|-)?\d+(\.\d+)?\)$','match')))
                 tipo=1;  %forma par ordenado              
               elseif(~isempty(regexp(val,'^\[(+|-)?\d+(\.\d+)?;(+|-)?\d+(\.\d+)?\]$','match')))
                 tipo=2; %formapolar
@@ -50,68 +50,138 @@ classdef NumeroComplejo<handle
             if(real ==0 && img<0)
                 ang =3*pi/2;
             end
-            if(real >0 && img==0)
+            if(real >=0 && img==0) 
                 ang = 0;
             end
             if(real <0 && img==0)
                 ang =pi;
             end      
-            thisnumeroComplejo.FomrPolar_fi = ang;
+            thisnumeroComplejo.FormPolar_fi = ang;
             thisnumeroComplejo.AnguloGrados =round(ang*180/pi);
         end
+        
         %cálculo módulo
         function mod = getModulo(thisnumeroComplejo,real, img)
           mod=sqrt(real^2+img^2);
           thisnumeroComplejo.FormPolar_ro = mod;
         end
-       %convertir Polar a Binomica    
-        function str = pol2binomica(thisnumeroComplejo, mod,ang)           
+        
+       %convierte Polar a Binomica y asigna el valor a la propiedado de la
+       %clase
+        function [real,img] = pol2binomica(thisnumeroComplejo, mod,ang)           
              real=mod*cos(ang);
-             img=mod*sin(ang);  
+             img=mod*sin(ang);
+
              %forma abreviada pero solo devuelve i
             % str= strcat(num2str(mod*exp(ang*j)));
-          str = strcat((num2str(real) ),';', num2str(img), 'j');
+          %str = strcat('(',(num2str(real) ),',', num2str(img),')');
+          
         end
+        %pasa (a,b) a la estructura a+bj  
+        function [bin] = parordenado2binomica(thisnumeroComplejo, real,img)
+%para cuando el numero complejo no quede 1+(-)j y quede directamente 1-j    
+             if img>=0
+                bin=strcat(num2str(real),'+', num2str(img), 'j');
+             else
+                bin=strcat(num2str(real),'-', num2str(abs(img)), 'j');
+             end
+         
+        end
+        
+        
+        
+    %realiza el conjugado en formato string            
+        function [bin] = conjugado(thisnumeroComplejo, real,img)
+           
+             if img>=0
+                bin=strcat(num2str(real),'-', num2str(img), 'j');
+             else
+                bin=strcat(num2str(real),'+', num2str(abs(img)), 'j');
+             end
+         
+        end
+        
+        
+        
+        
+        
+       
         %convertir de Binomica a polar
-        function str = bin2polar(thisnumeroComplejo, real,img )
+        %solo completa la propiedad de la clase en numero, la parte de
+        %string lo realiza en el constructor
+        function [mod,ang] = bin2polar(thisnumeroComplejo, real,img )
           mod = getModulo(thisnumeroComplejo,real,img);
           ang=  getAngulo(thisnumeroComplejo,real,img);  
           %angulo en grados
-          ang = ang*180/pi;
-          str = strcat('[',(num2str(mod) ),';', num2str(ang), ']');
+         % ang = ang*180/pi;
+          %str = strcat('[',(num2str(mod) ),';', num2str(ang), ']');
         end
         % \['+   +'\]
+        
+        
+        
         %constructor
-        function obj=NumeroComplejo( val)
+        function obj=NumeroComplejo(val)
+            if nargin==1 && isnumeric(val)==0
             %validar que val es complejo
-            dimeTipoComplejo(obj, val)            
+                dimeTipoComplejo(obj, val);            
             %realizar conversiones y asignar atributos
             %forma par ordenado 
-            if(obj.TipoInicial==1)
-              obj.FormPolar_string= val;
-              firstPart = regexp(val,'(+|-)?\d+(\.\d+)?;','match');
-              secondPart = regexp(val,'(+|-)?\d+(\.\d+)?\)','match');
-              obj.FormBinomica_real = str2double(regexprep( firstPart,';','','ignorecase'));
-              obj.FormBinomica_img = str2double(regexprep(secondPart,'\)','','ignorecase'));
-            end
+                if(obj.TipoInicial==1)
+                    obj.FormParOrdenado_string= val;
+                    firstPart = regexp(val,'(+|-)?\d+(\.\d+)?,','match');
+                    secondPart = regexp(val,'(+|-)?\d+(\.\d+)?\)','match');
+                    obj.FormBinomica_real = str2double(regexprep( firstPart,',','','ignorecase'));
+                    obj.FormBinomica_img = str2double(regexprep(secondPart,'\)','','ignorecase'));
+                   %completo las propiedades con las conversiones
+                    [mod,ang]=bin2polar(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                    obj.FormPolar_ro = mod;
+                    obj.FormPolar_fi = ang;
+                    
+                    obj.FormPolar_string=strcat('[',(num2str(mod) ),';', num2str(ang), ']');
+                    obj.FormBinomica_string=parordenado2binomica(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                    obj.AnguloGrados= round(ang*180/pi);
+                    obj.Conjugado_string=conjugado(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                end
             %formapolar
-            if(obj.TipoInicial==2)
-               obj.FormPolar_string=val; 
-               firstPart = regexp(val,'(+|-)?\d+(\.\d+)?;','match');
-               secondPart = regexp(val,'(+|-)?\d+(\.\d+)?\]','match');
-               obj.FormPolar_ro = str2double(regexprep( firstPart,';','','ignorecase'));
-               obj.FomrPolar_fi = str2double(regexprep(secondPart,'\]','','ignorecase'));
-               obj.AnguloGrados= round(obj.FomrPolar_fi*180/pi);
-            end
+                if(obj.TipoInicial==2)
+                    obj.FormPolar_string=val; 
+                    firstPart = regexp(val,'(+|-)?\d+(\.\d+)?;','match');
+                    secondPart = regexp(val,'(+|-)?\d+(\.\d+)?\]','match');
+                    obj.FormPolar_ro = str2double(regexprep( firstPart,';','','ignorecase'));
+                    obj.FormPolar_fi = str2double(regexprep(secondPart,'\]','','ignorecase'));
+                    obj.AnguloGrados= round(obj.FormPolar_fi*180/pi);
+
+                   %completo las propiedades con las conversiones
+                    [real_,img]=pol2binomica(obj,obj.FormPolar_ro,obj.FormPolar_fi);
+                    obj.FormBinomica_real = real_;
+                    obj.FormBinomica_img = img;
+                    obj.FormParOrdenado_string=strcat('(',(num2str(real_) ),',', num2str(img), ')');
+                    obj.FormBinomica_string=parordenado2binomica(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                    obj.Conjugado_string=conjugado(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                    
+                end
             %forma binomica
-            if(obj.TipoInicial==3)
-               obj.FormBinomica_string=val;
-               obj.FormBinomica_real = real(str2double(val));
-               obj.FormBinomica_img = imag(str2double(val));
-              obj.FormPolar_string = bin2polar(obj, obj.FormBinomica_real,obj.FormBinomica_img );
-            end
-            if(obj.TipoInicial==0)
-                error('no es un numero complejo');
+                if(obj.TipoInicial==3)
+
+                    obj.FormBinomica_string=val;
+                    obj.FormBinomica_real = real(str2double(val));
+                    obj.FormBinomica_img = imag(str2double(val));
+
+                   %completo las propiedades con las conversiones
+                   obj.FormParOrdenado_string=strcat('(',num2str(obj.FormBinomica_real),',', num2str(obj.FormBinomica_img ), ')');
+                    [mod,ang]=bin2polar(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                    obj.FormPolar_ro = mod;
+                    obj.FormPolar_fi = ang;
+                    obj.FormPolar_string=strcat('[',(num2str(mod) ),';', num2str(ang), ']');
+                    obj.AnguloGrados= round(ang*180/pi);
+                    conjugado(obj,obj.FormBinomica_real,obj.FormBinomica_img);
+                end
+               if(obj.TipoInicial==0)
+                    disp('no es un numero complejo');
+               end
+            else
+                disp('no ingreso parametro, o ingreso un parametro que no es string');
             end
         end
     end    
