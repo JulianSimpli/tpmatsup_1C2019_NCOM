5087461d24970ad264a27a03d7f65924c34a6d60
jorgemtnz <jorgeemtnz@gmail.com>
2019-04-26 19:24:54 -0300

    Se agrega Clase Operaciones para opraciones basicas y avanzadas. Se implementa operacion suma entre complejos. Se suben los test realizados

---
+++ Operaciones.m
@@ -0,0 +1,33 @@
+classdef Operaciones
+    %Clase para operaciones basicas y avanzadas
+    %referencia a NumeroComplejo
+    
+    %resultado es otro numero complejo.
+    
+    properties
+        
+    end
+    
+    methods
+        %suma de numeros complejos
+       function result=suma(thisoperaciones, numeroComplejo, otroNumeroComplejo)
+        real= numeroComplejo.FormBinomica_real + otroNumeroComplejo.FormBinomica_real;
+        img = numeroComplejo.FormBinomica_img + otroNumeroComplejo.FormBinomica_img;
+        str=strcat('(',(num2str(real) ),',', num2str(img), ')');
+        result = NumeroComplejo(str);
+       end
+        %resta de numeros complejos
+        
+        %multiplicacion de numeros complejos
+        
+        %cociente o division de numeros complejos
+        
+        %potencia
+        
+        %radicacion numero natural       
+        
+        
+    end
+    
+end
+
---
+++ TESTs/TestOB.m
@@ -0,0 +1,20 @@
+%test numeros complejos - operaciones basicas
+%para ejecutar dar clic en el boton triangunlo verde run
+function algo = TestOB()
+fprintf('test de operaciones basicas de numero complejo.\n');
+fprintf('Se crean dos numeros complejos y luego se suman.\n');
+a= NumeroComplejo('(3,50)')
+b= NumeroComplejo('(10,-7)')
+op=Operaciones;
+fprintf('El resultado será otro numero complejo.\n');
+c=suma(op,a,b)
+
+clear a b c;
+
+a = NumeroComplejo('5-12j')
+b= NumeroComplejo('[10;5.2356]')
+op=Operaciones;
+fprintf('El resultado será otro numero complejo.\n');
+c=suma(op,a,b)
+clear a b c;
+end
