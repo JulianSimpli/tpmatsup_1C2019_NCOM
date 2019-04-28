b8a0f42f7d48cd1dd4c393fc55027b5b770ba55e
Matias Nigliazzo <49684313+mnigliazzo@users.noreply.github.com>
2019-04-28 12:14:24 -0300

    Agregue mas pruebas
    Division y potencia

--- TESTs/TestOB.m
+++ TESTs/TestOB.m
@@ -1,13 +1,11 @@
-%test numeros complejos - operaciones basicas
-%para ejecutar dar clic en el boton triangunlo verde run
 function algo = TestOB()
 fprintf('test de operaciones basicas de numero complejo.\n');
 fprintf('Se crean dos numeros complejos y luego se suman.\n');
-a= NumeroComplejo('(3,50)')
-b= NumeroComplejo('(10,-7)')
-op=Operaciones;
-fprintf('El resultado será otro numero complejo.\n');
-c=suma(op,a,b)
+% a= NumeroComplejo('(3,50)')
+% b= NumeroComplejo('(10,-7)')
+% op=Operaciones;
+% fprintf('El resultado será otro numero complejo.\n');
+% c=suma(op,a,b)
 
 clear a b c;
 
@@ -36,4 +34,12 @@ fprintf('El resultado será otro numero complejo.\n');
 c=multiplicacion(op,a,b)
 clear a b c;
 
-end
+
+fprintf('Se crean dos numeros complejos y luego se dividen.\n');
+
+a = NumeroComplejo('-3+5j')
+b= NumeroComplejo('1-2j')
+op=Operaciones;
+fprintf('El resultado será otro numero complejo.\n');
+c=division(op,a,b)
+clear a b c;
